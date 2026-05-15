25a5929ce5feb1fd1932629339c81792002b82f6
thiagoo-carba <thiagoocarba@gmail.com>
2026-05-15 18:52:42 -0300

    Add initial HTML and JavaScript files for Ejercicio 9

---
+++ 3-Mayo/Ejercicio_9/script.js
@@ -0,0 +1,95 @@
+/*alert("Hola mundo!");
+
+var nombre = prompt("Nombre tuyo: ");
+
+alert("Hola, " + nombre + "!");
+
+var num1 = prompt("Introduce el primer número: ");
+var num2 = prompt("Introduce el segundo número: ");
+
+var resultado = parseInt(num1) + parseInt(num2);
+
+alert("El resultado de la suma es: " + resultado);
+
+var numero = prompt("Introduce un número: ");
+
+if (numero >= 18) {
+    alert("Eres mayor de edad.");
+} else {
+    alert("Eres menor de edad.");
+}
+
+var numero = prompt("Introduce un número: ");
+
+if (numero % 2 === 0) {
+    alert("El número es par.");
+}else {
+    alert("El número es impar.");
+}
+
+var num1 = parseInt(prompt("Ingresar un numero: "));
+var num2 = parseInt(prompt("Ingresar un numero: "));
+var num3 = parseInt(prompt("Ingresar un numero: "));
+
+var max = parseInt(0);
+
+if(num1 > num2 && num1 > num3){
+    alert(num1);
+}
+
+if(num2 > num1 && num2 > num3){
+    alert(num2);
+}
+
+if(num3 > num1 && num3 > num2){
+    alert(num3);
+}
+
+var nota = parseInt(prompt("Ingrese nota: "))
+if(nota < 5 && nota >=1 ){
+    alert("insuficiente")
+}else if (nota > 5 && nota < 8){
+    alert("Aceptable")
+}else{
+    alert("Muy bien")
+}
+
+for(var i = 0; i <= 10; i++){
+   alert(i);
+}
+
+for(var i = 0; i <= 20; i++){
+   if(i % 2 == 0){
+        alert(i);
+   }
+}
+
+var input = prompt("Contra:");
+
+if(input == 1234){
+    alert("Correcta");
+}else{
+    alert("Incorrecta");
+}
+
+let input = -1;
+let sumatotal = 0;
+
+
+while(input != 0){
+    input = parseInt(prompt("Ingrese un numero: "));
+    sumatotal += input;
+}
+
+alert("Suma total: " + sumatotal);
+
+*/
+
+let input = parseFloat(prompt("Ingrese un numero: "));
+let input2 = parseFloat(prompt("Ingrese un numero: "));
+let resultado = 0;
+
+
+if(input == 1){
+    
+}
