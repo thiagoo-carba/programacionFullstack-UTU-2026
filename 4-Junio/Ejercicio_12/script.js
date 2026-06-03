bf6865b32ee8e0785218b2db73f99a32cf719fc6
thiagoo-carba <thiagoocarba@gmail.com>
2026-06-03 15:49:47 -0300

    Add Ejercicio 12: Implement HTML structure, CSS styling, and JavaScript functionality for personal collection management

---
+++ 4-Junio/Ejercicio_12/script.js
@@ -0,0 +1,10 @@
+const agregar = document.querySelector(".agregar");
+
+agregar.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    const tituloInput = document.querySelector(".titulo");
+    const urlInput = document.querySelector(".URL");
+    const estadoInput = document.querySelector(".estado");
+    const calificacionInput = document.querySelector(".calificacion");
+}); 
