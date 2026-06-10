fcc6ba8b7f2c170c5f85b4091403ad432e89a8c1
thiagoo-carba <thiagoocarba@gmail.com>
2026-06-10 18:31:01 -0300

    Refactor JavaScript functionality to enhance cat image loading and interaction features

--- 4-Junio/Ejercicio_14/script.js
+++ 4-Junio/Ejercicio_14/script.js
@@ -1,6 +1,12 @@
 const img = document.querySelector(".fotogato");
 const btn = document.querySelector(".btn-gato");
-btn.addEventListener("click", async ()=> {
+const btnlike = document.querySelector(".btn1");
+const btndislike = document.querySelector(".btn2");
+const alerta = document.querySelector(".alerta");
+
+async function cargarGato() {
+        
+   
     try {
         const respuesta = await fetch(
             "https://api.thecatapi.com/v1/images/search",
@@ -17,8 +23,35 @@ btn.addEventListener("click", async ()=> {
         
 
     } catch (error) {
-        console.error(error);
+        alerta.textContent = "Error de comunicacion con la API";
     }
+
+}
+
+cargarGato();
+
+btn.addEventListener("click",  ()=> {
+    cargarGato();
+});
+
+const contador1 = document.querySelector(".contador1")
+let contadorlike = 0;
+
+btnlike.addEventListener("click", () => {
+contadorlike ++;
+
+contador1.textContent=contadorlike;
+cargarGato();
 
 });
 
+const contador2 = document.querySelector(".contador2")
+let contadordislike = 0;
+
+btndislike.addEventListener("click", () => {
+contadordislike ++;
+
+contador2.textContent=contadordislike;
+cargarGato();
+
+});
